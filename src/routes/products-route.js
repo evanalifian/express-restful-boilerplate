7708a6d8567f45413304d2d9d2d1ac087d921246
evanalifian <evanalifian@gmal.com>
2024-06-24 17:56:53 +0700

    change name file from api-route.js to products-route.js

---
+++ src/routes/products-route.js
@@ -0,0 +1,10 @@
+import express from "express"
+import productController from "../controllers/products-controller.js"
+
+export const productsRouter = express.Router()
+
+productsRouter.post('/', productController.create)
+productsRouter.get('/', productController.getAll)
+productsRouter.get('/:productId', productController.getById)
+productsRouter.put('/:productId', productController.update)
+productsRouter.delete('/:productId', productController.remove)
