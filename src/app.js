6e42e532284452be3a20604381e95364f7afc1e0
evanalifian <evanalifian@gmal.com>
2024-06-24 17:57:51 +0700

    set the route middleware as the same purpose

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from "express"
-import { APIRouter } from "./routes/api-route.js"
+import { productsRouter } from "./routes/products-route.js"
 import { errorMiddleware } from "./middlewares/error-middleware.js"
 import { notFound } from "./middlewares/not-found-middleware.js"
 
 export const app = express()
 app.use(express.json())
 
-app.use(APIRouter)
+app.use('/api/products', productsRouter)
 
 app.use(errorMiddleware)
 app.use(notFound)
