da9c897744fe5f01480f112f1707fc1ee207eb10
evanalifian <evanalifian@gmal.com>
2024-07-01 20:45:42 +0700

    tidy up the code to make it neat and add lines of code for the response

--- src/middlewares/error-middleware.js
+++ src/middlewares/error-middleware.js
@@ -7,12 +7,19 @@ export const errorMiddleware = (err, req, res, next) => {
   }
 
   if (err instanceof ResponseError) {
-    res.status(err.status).json({
-        errors: err.message
-    }).end();
-  }  else {
-    res.status(500).json({
-        errors: err.message
-    }).end();
+    res
+      .status(err.status)
+      .json({
+        status: err.status,
+        message: err.message,
+      })
+      .end();
+  } else {
+    res
+      .status(500)
+      .json({
+        errors: err.message,
+      })
+      .end();
   }
-}
+};
